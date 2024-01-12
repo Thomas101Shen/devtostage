d3115b8977e702d822796eff341aad98e4884698
Thomas101Shen <jacksparrow@Jacks-MacBook-Pro-3.local>
2024-01-12 12:27:25 -0500

    fixing bugs for deploy

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.12f49f7f83e5b4e901405500dab82fac.js"
+  "/devtostage/precache-manifest.707ec83c2ec1753c68c41296406f953b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/devtostage/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
